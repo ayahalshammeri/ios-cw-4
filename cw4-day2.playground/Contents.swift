6e06cf7a1576ce07748ec7d3281ec990c6eff261
AyahAlshammeri <66526830+jumana-alshameri@users.noreply.github.com>
2022-03-06 17:29:16 +0300

    dn

---
+++ cw4-day2.playground/Contents.swift
@@ -0,0 +1,10 @@
+import UIKit
+import Foundation
+
+var greeting = "Hello, playground"
+var name = "Ayah"
+var favouriteNumber = "8"
+    var hobby = "knitting"
+    
+print("hi, im",name, "my favourite number is",favouriteNumber, "and my hobby is",hobby)
+
